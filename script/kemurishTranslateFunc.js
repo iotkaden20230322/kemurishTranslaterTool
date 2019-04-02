1fe163b4596f04a540e8c78030e17c80f5acfce5
master <41655930+team3star@users.noreply.github.com>
2019-04-02 22:10:57 +0900

    正規表現を使わない方法は今後考えます

--- script/kemurishTranslateFunc.js
+++ script/kemurishTranslateFunc.js
@@ -9,7 +9,7 @@ function trDo(){
 		    createArray(xhr.responseText);
 		    };
 
-		    xhr.open("get", "./data/"+document.forms[0].character[count].value+".csv", true);
+		    xhr.open("get", "./datatext,"+document.forms[0].character[count].value+".csv", true);
 		    xhr.send(null);
 		}
 		count--;
@@ -17,12 +17,19 @@ function trDo(){
 
 */
 	    text    = document.forms[0].input.value;
-        text    = text.replace("とても", "めっさ");
-        text    = text.replace("好き", "…好きだ。");
-        text    = text.replace("です", "なのナ");
-        text    = text.replace("ます", "ますニャ");
-				text    = text.replace("ました", "したじゃん");
+        text    = replaceAll(text,"とても", "めっさ");
+        text    = replaceAll(text,"好き", "…好きだ。");
+        text    = replaceAll(text,"です", "なのナ");
+        text    = replaceAll(text,"ます", "ますニャ");
+				text    = replaceAll(text,"ました", "ましたじゃん");
+				text    = replaceAll(text,"考えていない", "意味見ていない");
+				text    = replaceAll(text,"ムシ", "ヌシ(ムシ違う)");
         document.forms[0].output.value = text;
+}
+
+function replaceAll(str, beforeStr, afterStr){
+  var reg = new RegExp(beforeStr, "g");
+  return str.replace(reg, afterStr);
 }
 /*
 function createArray(csvData) {

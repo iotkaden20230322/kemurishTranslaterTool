bb117ef8cf47296e9837d2dd6415326025416453
master <41655930+team3star@users.noreply.github.com>
2019-04-04 16:34:54 +0900

    CSVの内容を確認できるように機能追加

--- script/kemurishTranslateFunc.js
+++ script/kemurishTranslateFunc.js
@@ -1,7 +1,8 @@
-function trDo(){
+function trDo(var logicChose){
 
   var array = new Array();
   var text = document.forms[0].input.value;
+  var text2 = "";
   var tmpTxt = "error";
 
   for(var count = 0; count <= 17; count++) {
@@ -10,55 +11,63 @@ function trDo(){
       if(array != null){
         for(var countdata=0; countdata<=array.length; countdata++){
           if(array[countdata]){
-              if(array[countdata][1]){
-                tmpTxt = array[countdata][1];
-              } else {
-                array[countdata][1] = tmpTxt;
-              }
+            if(array[countdata][1]){
+              tmpTxt = array[countdata][1];
+            } else {
+              array[countdata][1] = tmpTxt;
+            }
+
+            if(logicChose==1){
               text = replaceAll(text,array[countdata][0],array[countdata][1]);
+            }else{
+              text2 = text2 + array[countdata][0] + " -> " + array[countdata][1];
+            }
           }
         }
       }
     } else {
       console.log("./data/"+document.forms[0].character[count].value+".csv is false.");
     }
-    document.forms[0].output.value = text + " #kmrkst";
-  }
-}
-
-function replaceAll(str, beforeStr, afterStr){
-  var reg = new RegExp(beforeStr, "g");
-  return str.replace(reg, afterStr);
-}
-
-function getCsv(url){
-  //CSVファイルを文字列で取得。
-  var txt = new XMLHttpRequest();
-  txt.open('get', url, false);
-  txt.send();
-
-  //改行ごとに配列化
-  var arr = txt.responseText.split('\n');
-
-  //1次元配列を2次元配列に変換
-  var res = [];
-  for(var i = 0; i < arr.length; i++){
-    //空白行が出てきた時点で終了
-    if(arr[i] == '') break;
-
-    //","ごとに配列化
-    res[i] = arr[i].split(',');
-
-    for(var i2 = 0; i2 < res[i].length; i2++){
-      //数字の場合は「"」を削除
-      if(res[i][i2].match(/\-?\d+(.\d+)?(e[\+\-]d+)?/)){
-        res[i][i2] = parseFloat(res[i][i2].replace('"', ''));
-      }
+    if(logicChose==1){
+      document.forms[0].output.value = text + " #kmrkst";
+    }else{
+      document.forms[0].output.value = text2;
     }
   }
-  if(res.length > 0){
-    return res;
+
+  function replaceAll(str, beforeStr, afterStr){
+    var reg = new RegExp(beforeStr, "g");
+    return str.replace(reg, afterStr);
   }
 
-  return null;
-}
+  function getCsv(url){
+    //CSVファイルを文字列で取得。
+    var txt = new XMLHttpRequest();
+    txt.open('get', url, false);
+    txt.send();
+
+    //改行ごとに配列化
+    var arr = txt.responseText.split('\n');
+
+    //1次元配列を2次元配列に変換
+    var res = [];
+    for(var i = 0; i < arr.length; i++){
+      //空白行が出てきた時点で終了
+      if(arr[i] == '') break;
+
+      //","ごとに配列化
+      res[i] = arr[i].split(',');
+
+      for(var i2 = 0; i2 < res[i].length; i2++){
+        //数字の場合は「"」を削除
+        if(res[i][i2].match(/\-?\d+(.\d+)?(e[\+\-]d+)?/)){
+          res[i][i2] = parseFloat(res[i][i2].replace('"', ''));
+        }
+      }
+    }
+    if(res.length > 0){
+      return res;
+    }
+
+    return null;
+  }

0ed39bc0776c62f7582ed52ff3626a4fa57f1967
master <41655930+team3star@users.noreply.github.com>
2019-04-09 18:10:16 +0900

    チェックボックス一括操作

--- script/kemurishTranslateFunc.js
+++ script/kemurishTranslateFunc.js
@@ -71,3 +71,27 @@ function getCsv(url){
 
   return null;
 }
+
+// 「全て選択」チェックで全てにチェック付く
+  function AllChecked(){
+    var all = document.form.all.checked;
+    for (var i=0; i<document.form.test.length; i++){
+      document.form.test[i].checked = all;
+    }
+  }
+
+  // 一つでもチェックを外すと「全て選択」のチェック外れる
+  function　DisChecked(){
+    var checks = document.form.test;
+    var checksCount = 0;
+    for (var i=0; i<checks.length; i++){
+      if(checks[i].checked == false){
+        document.form.all.checked = false;
+      }else{
+        checksCount += 1;
+        if(checksCount == checks.length){
+          document.form.all.checked = true;
+        }
+      }
+    }
+  }
